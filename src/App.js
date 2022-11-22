05f670d578945c7255f34fb26c293adea809e3f8
Xasanboy070 <xasanboyorifjonov0707@gmail.com>
2022-11-22 16:59:47 +0500

    Email

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
+import "./App.css";
 
 function App() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_unqnvlb",
+        "template_b0x9ssp",
+        form.current,
+        "cd7TTJNhNkRe1Zgd-"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    e.target.reset();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Email</h1>
+      <form ref={form} onSubmit={sendEmail} action="">
+        <input name="user_name" type="text" placeholder="text" />
+        <input name="user_email" type="email" placeholder="text" />
+        <input name="subject" type="number" placeholder="number" />
+        <button type="submit">send email</button>
+      </form>
     </div>
   );
 }
